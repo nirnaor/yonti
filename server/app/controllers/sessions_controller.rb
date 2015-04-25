1113a15709f96a9c9a4c3683f44def7d6ff67499
Nir Naor <nirnaori@gmail.com>
2015-04-25 18:10:55 +0300

    Rendering the user on successful login.

--- server/app/controllers/sessions_controller.rb
+++ server/app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by_name(params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: {logged_in: true}
+      render json: user
     else
       render json: {}, :status => :unauthorized
     end
